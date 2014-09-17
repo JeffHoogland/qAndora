b18c575f5d306c660fc299b2da899a7d187525d3
Jeff Hoogland <jeffhoogland@linux.com>
2014-09-17 14:44:06 -0500

    Added some functionality

--- playerVLC/__init__.py
+++ playerVLC/__init__.py
@@ -1,1 +1,1 @@
-
+from playerVLC import *
--- qAndora.py
+++ qAndora.py
@@ -12,11 +12,49 @@ class MainWindow(QMainWindow, Ui_qAndora):
         self.setupUi(self)
         self.assignButtons()
         
+        self.radioPlayer = playerVLC.volcanoPlayer()
+        self.radioPlayer.auth( "jeffhoogland@linux.com", "turtlethesquirrel")
+        self.radioPlayer.setStation(self.radioPlayer.getStations()[0])
+        self.radioPlayer.setChangeCallBack( self.songChange )
+        self.radioPlayer.addSongs()
+        
     def assignButtons( self ):
         self.playPauseButton.setIcon(QIcon("images/pause.png"))
+        self.playPauseButton.clicked.connect(self.playPausePressed)
         self.skipButton.setIcon(QIcon("images/skip.png"))
+        self.skipButton.clicked.connect(self.skipPressed)
         self.loveButton.setIcon(QIcon("images/favorite.png"))
+        self.loveButton.clicked.connect(self.lovePressed)
         self.banButton.setIcon(QIcon("images/ban.png"))
+        self.banButton.clicked.connect(self.banPressed)
+    
+    def songChange( self ):
+        print "Song changed"
+        info = self.radioPlayer.songinfo[self.radioPlayer.curSong]
+        self.titleLabel.setText(info['title'])
+        self.albumLabel.setText(info['album'])
+        if info['rating'] == "love":
+            self.loveButton.setIcon(QIcon("images/love.png"))
+        else:
+            self.loveButton.setIcon(QIcon("images/favorite.png"))
+        
+    def playPausePressed( self ):
+        if self.radioPlayer.playing:
+            self.radioPlayer.pauseSong()
+            self.playPauseButton.setIcon(QIcon("images/play.png"))
+        else:
+            self.radioPlayer.playSong()
+            self.playPauseButton.setIcon(QIcon("images/pause.png"))
+    
+    def skipPressed( self ):
+        self.radioPlayer.skipSong()
+    
+    def lovePressed( self ):
+        self.radioPlayer.loveSong()
+        self.loveButton.setIcon(QIcon("images/love.png"))
+        
+    def banPressed( self ):
+        self.radioPlayer.banSong()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
