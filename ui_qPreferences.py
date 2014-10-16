865a257cdc511e31195de14f8deee6e198cd4bc9
Jeff Hoogland <jeffhoogland@linux.com>
2014-10-15 23:18:13 -0500

    Optimizing mobile UI

--- ui_qPreferences.py
+++ ui_qPreferences.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'qPreferences.ui'
 #
-# Created: Wed Oct 15 23:05:47 2014
+# Created: Wed Oct 15 23:17:46 2014
 #      by: pyside-uic 0.2.15 running on PySide 1.2.1
 #
 # WARNING! All changes made in this file will be lost!
